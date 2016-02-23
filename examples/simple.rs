766f5f25353f989502991475ae70945a827fdc90
Jonathan Reem <jonathan.reem@gmail.com>
2016-02-22 22:17:29 -0800

    Simplify the example and copy it to examples.
    Examples in the examples folder are compiled as part of `cargo test`
    so they are much less likely to bitrot.

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,34 +1,3 @@
-# libkeccak-tiny
-
-An implementation of the FIPS-202-defined SHA-3 and SHAKE functions.
-
-The `Keccak-f[1600]` permutation is fully unrolled; it's nearly as fast
-as the Keccak team's optimized permutation.
-
-## Building
-
-```bash
-cargo build
-```
-
-## Usage
-
-Add this to your `Cargo.toml`:
-
-```toml
-[dependencies]
-tiny-keccak = "1.0"
-```
-
-and this to your crate root:
-
-```rust
-extern crate tiny_keccak;
-```
-
-## Example
-
-```rust
 extern crate tiny_keccak;
 use tiny_keccak::Keccak;
 
@@ -53,4 +22,4 @@ fn main() {
 
     assert_eq!(&res, expected);
 }
-```
+
